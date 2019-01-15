cd839eb61977e709979976583044ab0d9c89641c
gaoshengnan <16619974352@163.com>
2019-01-15 22:43:46 +0800

    判断链表是否有环，并返回入环结点

---
+++ src/main/java/linkedlist/linkedListCycleII/LinkedListCycleII.java
@@ -0,0 +1,41 @@
+package linkedlist.linkedListCycleII;
+
+import linkedlist.ListNode;
+
+/**
+ * @author Seina
+ * @version 2019-01-15 21:07:00
+ */
+public class LinkedListCycleII {
+    public ListNode detectCycle(ListNode head) {
+        if (head == null || head.next == null){
+            return null;
+        }
+        ListNode fast = head;
+        ListNode low = head;
+
+        //标识是否有环
+        boolean hasCycle = false;
+        while (fast != null && fast.next != null){
+            fast = fast.next.next;
+            low = low.next;
+            if (fast == low){
+                hasCycle = true;
+                break;
+            }
+        }
+
+        //如果有环，求出入环结点
+        if (hasCycle){
+            fast = head;
+            while (fast != low){
+                fast = fast.next;
+                low = low.next;
+            }
+            return low;
+        }
+
+        ///如果没有环，返回null
+        return null;
+    }
+}
