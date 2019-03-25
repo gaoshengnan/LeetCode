719da921e2ce03d0fc1e419bdd74471b417b48ea
gaoshengnan <16619974352@163.com>
2019-03-26 00:26:11 +0800

    update REDAMEME.me

---
+++ src/main/java/stack/ImplementQueueUsingStacks/ImplementQueueUsingStacks.java
@@ -0,0 +1,43 @@
+package stack.ImplementQueueUsingStacks;
+
+import java.util.Stack;
+
+/**
+ * @author Seina
+ * @version 2019-02-08 18:16:59
+ */
+public class ImplementQueueUsingStacks {
+
+    //用两个栈互相倒实现队列
+
+    Stack<Integer> stackA = new Stack<Integer>();
+    Stack<Integer> stackB = new Stack<Integer>();
+
+
+    /** Push element x to the back of queue. */
+    /** 将元素推到队列的尾部--->往队列里面添加一个元素（push往尾部推） */
+    public void push(int x) {
+        stackA.push(x);
+    }
+
+    /** Removes the element from in front of queue and returns that element. */
+    /** 从队列首部移除元素，并且返回这个元素--->移除元素（pop从首部往外蹦） */
+    public int pop() {
+        for (int i : stackA) {
+            stackB.push(stackA.pop());
+        }
+        return stackB.pop();
+    }
+
+    /** Get the front element. */
+    /** 得到队列首部的元素 */
+    public int peek() {
+        return stackB.pop();
+    }
+
+    /** Returns whether the queue is empty. */
+    /** 返回队列是否为null */
+    public boolean empty() {
+        return true;
+    }
+}
---
+++ src/main/java/stack/validParentheses/ValidParentheses.java
@@ -0,0 +1,44 @@
+package stack.validParentheses;
+
+/**
+ * @author Seina
+ * @version 2019-01-25 20:58:04
+ *
+ * LeetCode20：有效的括号
+ */
+public class ValidParentheses {
+    public static boolean isValid(String s) {
+        if(s == null || s.length() == 1) return false;
+        if(s.length() == 0) return true;
+
+        //toCharArray声明了一个字符串长度那么大的数组arr
+        char[] arr = s.toCharArray();
+        if (arr[0] == ')' || arr[0] == ']' || arr[0] == '}') return false;
+
+        //又声明了一个比字符串长度大1的数组stack（可以理解成底层是数组实现的顺序栈）
+        int[] stack = new int[arr.length+1];
+        int ptr = 0;
+
+        //开始遍历数组arr
+        for (int i = 0; i < arr.length; i++) {
+            int val = 0;
+            switch(arr[i]){
+                case '(':val = -1;break;
+                case ')':val = 1;break;
+                case '[':val = -2;break;
+                case ']':val = 2;break;
+                case '{':val = -3;break;
+                case '}':val = 3;break;
+                default:break;
+            }
+            if(stack[ptr] + val == 0){
+                stack[ptr--] = 0;
+            }else{
+                stack[++ptr] = val;
+            }
+        }
+        return stack[1] == 0;
+    }
+
+
+}
