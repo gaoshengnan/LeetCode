0d8ed3ca41f1041a82b04652862e54334b13e61b
gaoshengnan <16619974352@163.com>
2019-04-01 22:11:10 +0800

    栈是一种’操作受限’的线性表，只允许在一端插入和删除数据，并且满足后进先出，先进后出的特性。

---
+++ src/main/java/stack/ArrayStack.java
@@ -0,0 +1,48 @@
+package stack;
+
+/**
+ * @author Seina
+ * @version 2019-03-28 17:30:01
+ *
+ * 用数组实现一个栈，又称顺序栈
+ */
+public class ArrayStack {
+
+    private String[] items;//数组
+    private int count;//栈中元素个数
+    private int n;//栈的大小
+
+
+    //初始化数组，申请一个大小为n的数组空间
+    private ArrayStack(int n) {
+        this.items = new String[n];
+        this.count = 0;
+        this.n = n;
+    }
+
+    //入栈
+    public boolean push(String item) {
+        if (count == n) return false;
+        items[count] = item;
+        count++;
+        return true;
+    }
+
+    //出栈，返回出栈元素
+    public String pop() {
+        if (count == 0) return null;
+        String temp = items[count-1];
+        --count;
+        return temp;
+    }
+
+    public static void main(String[] args) {
+        ArrayStack stack = new ArrayStack(5);
+        stack.push("haha");
+        stack.push("hehe");
+        stack.push("yaya");
+        String pop = stack.pop();
+        int count = stack.count;
+
+    }
+}
