bbe9d9ab7e9069021bcdb956dffc89e3f3c1e63d
shengnan.gao <16619974352@163.com>
2019-04-18 23:11:04 +0800

    <linkedlist>delete duplicates from sorted list

---
+++ src/main/java/linkedlist/leetCode83/RemoveDuplicatesFromSortedList.java
@@ -0,0 +1,44 @@
+package linkedlist.leetCode83;
+
+import linkedlist.ListNode;
+
+/**
+ * @author Seina
+ * @version 2019-04-18 22:52:45
+ */
+public class RemoveDuplicatesFromSortedList {
+
+    /*
+     * Given a sorted linked list, delete all duplicates such that each element appear only once.
+     *
+     * Example 1:
+     *
+     * Input: 1->1->2
+     * Output: 1->2
+     *
+     * Example 2:
+     *
+     * Input: 1->1->2->3->3
+     * Output: 1->2->3
+     *
+     */
+
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode cur = head;
+
+        while (cur.next != null) {
+            if (cur.data == cur.next.data) {
+                cur.next = cur.next.next;
+            } else {
+                cur = cur.next;
+            }
+        }
+        return head;
+    }
+
+
+
+}
