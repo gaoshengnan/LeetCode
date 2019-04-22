76ee269c6fbf59a8cdf328d3023122f5dab9787d
shengnan.gao <16619974352@163.com>
2019-04-22 23:17:01 +0800

    readme - add skip list

---
+++ src/main/java/linkedlist/leetCode21/MergeTwoSortedLists.java
@@ -0,0 +1,30 @@
+package linkedlist.leetCode21;
+
+import linkedlist.ListNode;
+
+/**
+ * @author Seina
+ * @version 2019-04-19 08:20:26
+ */
+public class MergeTwoSortedLists {
+
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+
+        /*
+         * plan a
+         * insert a linked list into anther linked list
+         * l1 - > l2
+         */
+        if (l1 == null) return l2;
+        if (l2 == null) return l1;
+
+        /*
+         * plan b
+         *
+         * insert two linked list into a new linked list
+         */
+
+
+        return l1;
+    }
+}
