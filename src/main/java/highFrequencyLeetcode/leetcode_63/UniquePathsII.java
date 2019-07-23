e2236410cb72f253dca22725764348af3523dd70
gaoshengnan <16619974352@163.com>
2019-07-23 22:44:55 +0800

    feat: leetcode63 - unique paths II

--- src/main/java/highFrequencyLeetcode/leetcode_63/UniquePathsII.java
+++ src/main/java/highFrequencyLeetcode/leetcode_63/UniquePathsII.java
@@ -9,8 +9,6 @@ package highFrequencyLeetcode.leetcode_63;
  *
  *  现在考虑网格中有障碍物。那么从左上角到右下角将会有多少条不同的路径？
  *
- *
- *
  *  网格中的障碍物和空位置分别用 1 和 0 来表示。
  *
  *  说明：m 和 n 的值均不超过 100。
@@ -36,4 +34,28 @@ package highFrequencyLeetcode.leetcode_63;
  * @version 2019-07-23 17:01:38
  */
 public class UniquePathsII {
+
+    /**
+     * 解法1 动态规划
+     *
+     * 解题思路类似不同路径将 62，这里需要特殊处理的是，要将障碍物的位置变成 0
+     *
+     * 时间复杂度：O(m*n)
+     * 空间复杂度：O(m*n)
+     *
+     * @param obstacleGrid: 给定包含障碍物的二维表格
+     * @return 共有多少种不同路径
+     */
+    public int uniquePathsWithObstacles (int[][] obstacleGrid) {
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+        obstacleGrid[0][0] ^= 1;//起始位置 0
+        //只要前一个元素是障碍物，后面的第一行全部是 0，因为不能从上面过来，左边又是障碍物
+        for (int i = 1; i < m; i++) obstacleGrid[i][0] = (obstacleGrid[i][0] == 1) ? 0 : obstacleGrid[i - 1][0];
+        for (int j = 1; j < n; j++) obstacleGrid[0][j] = (obstacleGrid[0][j] == 1) ? 0 : obstacleGrid[0][j - 1];
+        for (int i = 1; i < m; i++)
+            for (int j = 1; j < n; j++)
+                obstacleGrid[i][j] = (obstacleGrid[i][j] == 1) ? 0 : obstacleGrid[i - 1][j] + obstacleGrid[i][j - 1];
+        return obstacleGrid[m - 1][n - 1];
+    }
 }
